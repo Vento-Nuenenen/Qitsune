89b3bd97ad97c9e033d58d65837a4194f4a6d52c
Chronyms <caspar.brenneisen@protonmail.com>
2017-08-27 14:08:27 +0000

    Apply fixes from StyleCI

--- database/migrations/2017_08_27_134457_game_codes.php
+++ database/migrations/2017_08_27_134457_game_codes.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class GameCodes extends Migration
 {
@@ -13,11 +13,11 @@ class GameCodes extends Migration
      */
     public function up()
     {
-	    Schema::create('game_codes',function(Blueprint $table){
-		    $table->increments('id');
-		    $table->uuid('game_code');
-		    $table->integer('points');
-	    });
+        Schema::create('game_codes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('game_code');
+            $table->integer('points');
+        });
     }
 
     /**
@@ -27,6 +27,6 @@ class GameCodes extends Migration
      */
     public function down()
     {
-	    Schema::dropIfExists('game_codes');
+        Schema::dropIfExists('game_codes');
     }
 }
