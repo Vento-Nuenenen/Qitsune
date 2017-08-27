a6343945f6b20d48235c6c73a064197ac9dfb607
Chronyms <chronyms@outlook.com>
2017-08-27 19:38:08 +0200

    Add Gamerules
    Add Gamerules

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
@@ -14,7 +14,18 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <div class="welcome">
+                        Hallo {{ ((Auth::user()->scoutname != null) ? Auth::user()->scoutname : Auth::user()->prename) }}. <br/>
+                        Dein Login-Name ist <b>{{ ((Auth::user()->scoutname != null) ? Auth::user()->prename."_".Auth::user()->scoutname."_".Auth::user()->surname : Auth::user()->prename."_".Auth::user()->surname) }}</b>.
+                        Merk dir diesen, damit du dich später wieder Einloggen kannst.
+                    </div>
+
+                    <div>
+                        <h1>Spielregeln</h1>
+                        Auf dem Gelände sind QR-Codes versteckt. Finde diese und Scanne sie mit deinem SmartPhone ein.
+                        Auf dem Admin-Monitor sieht du die aktuelle Rangliste, nach Anzahl gefundener QR-Codes.
+                    </div>
+
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ ((Auth::user()->scoutname != null) ? Auth::user()->prename." ".Auth::user()->scoutname." ".Auth::user()->surname : Auth::user()->prename." ".Auth::user()->surname) }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
