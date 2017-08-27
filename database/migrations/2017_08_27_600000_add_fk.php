c793b2da326ba8c93111846085ad8bd6ef1c751d
Chronyms <caspar.brenneisen@protonmail.com>
2017-08-27 15:26:09 +0000

    Apply fixes from StyleCI

--- database/migrations/2017_08_27_600000_add_fk.php
+++ database/migrations/2017_08_27_600000_add_fk.php
@@ -13,13 +13,13 @@ class AddFk extends Migration
      */
     public function up()
     {
-	    Schema::table('users_codes',function(Blueprint $table){
-		    $table->foreign('fk_users')->references('id')->on('users');
-		    $table->foreign('fk_game_codes')->references('id')->on('game_codes');
-	    });
+        Schema::table('users_codes', function (Blueprint $table) {
+            $table->foreign('fk_users')->references('id')->on('users');
+            $table->foreign('fk_game_codes')->references('id')->on('game_codes');
+        });
 
-	    Schema::table('users',function(Blueprint $table){
-		    $table->foreign('fk_role')->references('id')->on('role');
-	    });
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('fk_role')->references('id')->on('role');
+        });
     }
 }
