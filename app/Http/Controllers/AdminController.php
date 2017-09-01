54be5881cc82e29c096883a0e0e8784b9ba98298
Chronyms <caspar.brenneisen@protonmail.com>
2017-09-01 16:41:14 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,14 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AdminController extends Controller
 {
-	public function index()
-	{
-
-
-		return view('admin.admin');
-	}
+    public function index()
+    {
+        return view('admin.admin');
+    }
 }
