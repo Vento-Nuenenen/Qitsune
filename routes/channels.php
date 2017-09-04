46482268a07bbfec45c67d4f819126794df2ceba
Chronyms <chronyms@outlook.com>
2017-09-04 20:34:30 +0200

    Fix

--- routes/channels.php
+++ routes/channels.php
@@ -14,10 +14,3 @@
 Broadcast::channel('App.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-<<<<<<< HEAD
-=======
-
-Broadcast::channel('App.User.{role}', function ($user, $role) {
-    return (int) $user->role === (int) $role;
-});
->>>>>>> fd930b6d1b0828fd3371a8ae67e1f189ab314e01
