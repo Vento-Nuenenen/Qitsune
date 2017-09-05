6f62fb080ab18a55d8e8431ee6ff903c96817f8a
Chronyms <caspar.brenneisen@protonmail.com>
2017-09-05 19:56:06 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -13,35 +13,34 @@ class RankController extends Controller
      */
     private static function index()
     {
-	    $allUsers = DB::select('SELECT name_gen FROM users WHERE fk_role = 2;');
-	    $userArray = json_decode(json_encode($allUsers), true);
+        $allUsers = DB::select('SELECT name_gen FROM users WHERE fk_role = 2;');
+        $userArray = json_decode(json_encode($allUsers), true);
 
-	    $userCount = count($allUsers);
+        $userCount = count($allUsers);
 
-	    for($i = 0; $i < $userCount; ++$i){
-		    $rawRank = DB::select('SELECT points,name_gen FROM users LEFT JOIN users_codes ON users.id = users_codes.fk_users LEFT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE users_codes.fk_game_codes IS NOT NULL AND users.name_gen = ?;',array($userArray[$i]['name_gen']));
-		    $rankArray = json_decode(json_encode($rawRank), true);
+        for ($i = 0; $i < $userCount; ++$i) {
+            $rawRank = DB::select('SELECT points,name_gen FROM users LEFT JOIN users_codes ON users.id = users_codes.fk_users LEFT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE users_codes.fk_game_codes IS NOT NULL AND users.name_gen = ?;', [$userArray[$i]['name_gen']]);
+            $rankArray = json_decode(json_encode($rawRank), true);
 
-		    $rankCount = count($rankArray);
+            $rankCount = count($rankArray);
 
-		    $points = 0;
-		    for($j = 0; $j < $rankCount;++$j){
-		    	$points += $rankArray[$j]['points'];
-		    }
+            $points = 0;
+            for ($j = 0; $j < $rankCount; ++$j) {
+                $points += $rankArray[$j]['points'];
+            }
 
-		    DB::update('UPDATE users SET rank = ? WHERE name_gen = ?;',array($points,$rankArray[$i]['name_gen']));
-	    }
-
-
+            DB::update('UPDATE users SET rank = ? WHERE name_gen = ?;', [$points, $rankArray[$i]['name_gen']]);
+        }
     }
 
-    public static function show(){
-    	RankController::index();
+    public static function show()
+    {
+        self::index();
 
-    	$rankObj = DB::select('SELECT prename,scoutname,surname,rank  FROM users WHERE fk_role = 2 ORDER BY rank DESC;');
-	    $rankArray = json_decode(json_encode($rankObj), true);
-	    $userRank = count($rankArray);
+        $rankObj = DB::select('SELECT prename,scoutname,surname,rank  FROM users WHERE fk_role = 2 ORDER BY rank DESC;');
+        $rankArray = json_decode(json_encode($rankObj), true);
+        $userRank = count($rankArray);
 
-	    return view('home',['rankArray' => $rankArray,'userRank' => $userRank]);
+        return view('home', ['rankArray' => $rankArray, 'userRank' => $userRank]);
     }
 }
