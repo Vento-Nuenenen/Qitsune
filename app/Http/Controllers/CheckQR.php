7a20b745a69b114d1cd403493181f2130cdf1577
Chronyms <caspar.brenneisen@protonmail.com>
2017-09-17 18:35:30 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/CheckQR.php
+++ app/Http/Controllers/CheckQR.php
@@ -14,18 +14,18 @@ class CheckQR extends Controller
      */
     public function index($param)
     {
-    	if(Auth::check()){
-		    $rawQR = DB::select('SELECT * FROM game_codes WHERE game_code = ?;',[$param]);
-		    $checkExists = DB::select('SELECT * FROM users RIGHT JOIN users_codes ON users.id = users_codes.fk_users RIGHT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE name_gen = ? AND game_code = ?;',[Auth::user()->name_gen,$param]);
+        if (Auth::check()) {
+            $rawQR = DB::select('SELECT * FROM game_codes WHERE game_code = ?;', [$param]);
+            $checkExists = DB::select('SELECT * FROM users RIGHT JOIN users_codes ON users.id = users_codes.fk_users RIGHT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE name_gen = ? AND game_code = ?;', [Auth::user()->name_gen, $param]);
 
-		    if(empty($checkExists)){
-			    DB::table('users_codes')->insert(['fk_users' => Auth::user()->id,'fk_game_codes' => $rawQR[0]->id]);
-			    $checkExists = DB::select('SELECT * FROM users RIGHT JOIN users_codes ON users.id = users_codes.fk_users RIGHT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE name_gen = ? AND game_code = ?;',[Auth::user()->name_gen,$param]);
-		    }
+            if (empty($checkExists)) {
+                DB::table('users_codes')->insert(['fk_users' => Auth::user()->id, 'fk_game_codes' => $rawQR[0]->id]);
+                $checkExists = DB::select('SELECT * FROM users RIGHT JOIN users_codes ON users.id = users_codes.fk_users RIGHT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE name_gen = ? AND game_code = ?;', [Auth::user()->name_gen, $param]);
+            }
 
-		    return view('qr',['checkExists' => $checkExists]);
-        }else{
-		    return redirect()->route('login');
-	    }
+            return view('qr', ['checkExists' => $checkExists]);
+        } else {
+            return redirect()->route('login');
+        }
     }
 }
