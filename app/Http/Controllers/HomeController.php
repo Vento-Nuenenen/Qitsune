6a2272b9921e980619aa97bd467664563c3742fc
Chronyms <caspar.brenneisen@protonmail.com>
2017-09-23 20:18:40 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Auth;
 use Illuminate\Http\Request;
 use TCG\Voyager\Facades\Voyager;
-use Auth;
 
 class HomeController extends Controller
 {
@@ -13,16 +13,16 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function checkAuth(Request $request,DescriptionController $ds)
+    public function checkAuth(Request $request, DescriptionController $ds)
     {
-	    if(Auth::check() && Voyager::can('browse_admin')){
-		    return redirect()->action('RankingController@showRanking');
-	    }elseif(Auth::check()){
-		    return redirect()->action('DescriptionController@showDescription');
-	    }else{
-		    $request->session()->flash('error', 'Bitte Log dich ein oder Registrier dich!');
+        if (Auth::check() && Voyager::can('browse_admin')) {
+            return redirect()->action('RankingController@showRanking');
+        } elseif (Auth::check()) {
+            return redirect()->action('DescriptionController@showDescription');
+        } else {
+            $request->session()->flash('error', 'Bitte Log dich ein oder Registrier dich!');
 
-		    return redirect()->route('login');
-	    }
+            return redirect()->route('login');
+        }
     }
 }
