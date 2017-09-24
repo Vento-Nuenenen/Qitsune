23af3bdd0f6637b8ee2778f3e3e4e97b3c519632
Chronyms <caspar.brenneisen@protonmail.com>
2017-09-24 12:43:37 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -4,22 +4,23 @@ namespace App\Http\Controllers;
 
 class RankingController extends Controller
 {
-	/**
-	 * Create a new controller instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		$this->middleware('auth');
-	}
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-	/**
-	 * Show the ranking table
-	 *
-	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-	 */
-	public function showRanking(){
-	    //return view('user.description');
+    /**
+     * Show the ranking table.
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function showRanking()
+    {
+        //return view('user.description');
     }
 }
