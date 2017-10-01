8c8eac7b17df49c873668aed4a14437b2cd7f9cc
Chronyms <chronyms@outlook.com>
2017-10-01 22:20:39 +0200

    Try fix issue

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -2,47 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use function GuzzleHttp\Promise\promise_for;
 use Illuminate\Support\Facades\DB;
 
 class RankingController extends Controller
 {
-    public function showRanking()
-    {
-        self::index();
-
-        $rankObj = DB::select('SELECT first_name,scoutname,last_name,total_points  FROM users WHERE total_points > 0 ORDER BY total_points DESC;');
-        $rankArray = json_decode(json_encode($rankObj), true);
-        $userRank = count($rankArray);
-
-        return view('leader.ranking', ['rankArray' => $rankArray, 'userRank' => $userRank]);
-    }
-
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return void
+	 */
     private static function index()
     {
-        $allUsers = DB::select('SELECT name_gen FROM users;');
-        $userArray = json_decode(json_encode($allUsers), true);
+	    $allUsers = DB::select('SELECT name_gen FROM users;');
+	    $userArray = json_decode(json_encode($allUsers), true);
 
-        $userCount = count($allUsers);
+	    $userCount = count($allUsers);
 
-        for ($i = 1; $i < $userCount; ++$i) {
-            $rawRank = DB::select('SELECT points,name_gen FROM users LEFT JOIN users_codes ON users.id = users_codes.fk_users LEFT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE users_codes.fk_game_codes IS NOT NULL AND users.name_gen = ?;', [$userArray[$i - 1]['name_gen']]);
-            $rankArray = json_decode(json_encode($rawRank), true);
+	    foreach($userArray as $user) {
+		    $rawRank = DB::select('SELECT points,name_gen FROM users LEFT JOIN users_codes ON users.id = users_codes.fk_users LEFT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE users_codes.fk_game_codes IS NOT NULL AND users.name_gen = ?;', [$user['name_gen']]);
+		    $rankArray = json_decode(json_encode($rawRank), true);
 
-            $rankCount = count($rankArray);
+		    print_r($rankArray);
+		    $rankCount = count($rankArray);
 
-            $points = 0;
-            for ($j = 1; $j <= $rankCount; ++$j) {
-                $points += $rankArray[$j - 1]['points'];
-            }
+		    $points = 0;
+		    foreach($user['points'] as $point) {
+			    $points += $point['points'];
+		    }
 
-            if ($points > 0) {
-                DB::update('UPDATE users SET total_points = ? WHERE name_gen = ?;', [$points, $rankArray[$i - 1]['name_gen']]);
-            }
-        }
+		    if ($points > 0) {
+			    DB::update('UPDATE users SET rank = ? WHERE name_gen = ?;', [$points, $user['name_gen']]);
+		    }
+	    }
     }
+
+	/**
+	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+	 */
+	public function showRanking()
+	{
+		self::index();
+
+		$rankObj = DB::select('SELECT first_name,scoutname,last_name,rank  FROM users WHERE rank > 0 ORDER BY rank DESC;');
+		$rankArray = json_decode(json_encode($rankObj), true);
+		$userRank = count($rankArray);
+
+		return view('leader.ranking', ['rankArray' => $rankArray, 'userRank' => $userRank]);
+	}
+
 }
