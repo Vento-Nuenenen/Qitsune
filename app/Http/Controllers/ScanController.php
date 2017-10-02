ba3436ef70e89447107c7759c851103726221d3e
Chronyms <chronyms@outlook.com>
2017-10-02 21:28:37 +0200

    Fix git issue

--- app/Http/Controllers/ScanController.php
+++ app/Http/Controllers/ScanController.php
@@ -13,7 +13,6 @@ class ScanController extends Controller
 	 */
 	public function showScan($param)
     {
-<<<<<<< HEAD
         	$rawQR = DB::select('SELECT * FROM game_codes WHERE  game_code = ?', [$param]);
 		$checkExists = $this->checkCodeExists($param,Auth::user()->name_gen);
 
@@ -69,35 +68,5 @@ class ScanController extends Controller
 	private function getAllPointsPerUser($name_gen){
 	    $pointsPerUser = DB::select('SELECT points,name_gen FROM users LEFT JOIN users_codes ON users.id = users_codes.fk_users LEFT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE users.name_gen = ?;', [$name_gen]);
 	    return $pointsPerUserArray = json_decode(json_encode($pointsPerUser), true);
-=======
-        $rawQR = DB::select('SELECT * FROM game_codes WHERE  game_code = ?', [$param]);
-        $checkExists = $this->checkCodeExists($param, Auth::user()->name_gen);
-
-        if (count($rawQR) > 0) {
-            if (count($checkExists) < 1) {
-                DB::table('users_codes')->insert(['fk_users' => Auth::user()->id, 'fk_game_codes' => $rawQR[0]->id]);
-                $checkExists = DB::select('SELECT * FROM users RIGHT JOIN users_codes ON users.id = users_codes.fk_users RIGHT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE name_gen = ? AND game_code = ?;', [Auth::user()->name_gen, $param]);
-                $checkExists = json_decode(json_encode($checkExists));
-
-                return view('user.scan', ['checkExists' => $checkExists]);
-            } else {
-                $checkExists = DB::select('SELECT * FROM users RIGHT JOIN users_codes ON users.id = users_codes.fk_users RIGHT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE name_gen = ? AND game_code = ?;', [Auth::user()->name_gen, $param]);
-                $checkExists = json_decode(json_encode($checkExists));
-
-                return view('user.scan', ['checkExists' => $checkExists])->withErrors('Der Code wurde bereits gezählt.');
-            }
-        } else {
-            abort(404);
-        }
-    }
-
-    private function checkCodeExists($code, $name_gen)
-    {
-        return DB::select('SELECT * FROM users RIGHT JOIN users_codes ON users.id = users_codes.fk_users RIGHT JOIN game_codes ON users_codes.fk_game_codes = game_codes.id WHERE name_gen = ? AND game_code = ?;', [$name_gen, $code]);
-    }
-
-    private function calcTotalPoints()
-    {
->>>>>>> 763c96f71c69c25b6d732167fbb14d17dc60f52b
     }
 }
