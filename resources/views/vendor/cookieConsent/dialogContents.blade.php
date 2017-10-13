cd05091d83098953541fc708924f2932b243172b
Chronyms <chronyms@outlook.com>
2017-10-13 22:45:12 +0200

    Fix

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -2,7 +2,7 @@
     <span class="cookie-consent__message">
         {!! trans('cookieConsent::texts.message') !!}
     </span>
-    <button class="btn btn-primary">
+    <button class="btn btn-primary js-cookie-consent-agree cookie-consent__agree">
         {{ trans('cookieConsent::texts.agree') }}
     </button>
 </div>
