1bc822f413964d109bc255c3614907e7fff6f16b
Chronyms <chronyms@outlook.com>
2017-10-31 17:29:17 +0100

    Remove online sources

--- public/js/app.a765b1233a026fe153b7.js
+++ public/js/app.a765b1233a026fe153b7.js
@@ -12320,13 +12320,6 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-/* ========================================================================
- * Bootstrap: alert.js v3.3.7
- * http://getbootstrap.com/javascript/#alerts
- * ========================================================================
- * Copyright 2011-2016 Twitter, Inc.
- * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
- * ======================================================================== */
 
 
 +function ($) {
