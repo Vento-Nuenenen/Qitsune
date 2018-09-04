9900c34a5ab8f88c0c4446138729e758f60ca910
Vento-Nuenenen <35610192+Vento-Nuenenen@users.noreply.github.com>
2018-09-04 19:17:20 +0000

    Implement Database

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name_gen')->unique();
+	        $table->string('scout_name')->nullable();
+	        $table->string('first_name');
+	        $table->string('last_name');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
