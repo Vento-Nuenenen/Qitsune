ac330c7cfdb95e3095f231a52fe8463bdceef2bb
Chronyms <caspar.brenneisen@protonmail.com>
2018-09-11 18:29:47 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,39 +8,39 @@ use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
-	/**
-	 * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
-	 */
-	public function index()
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
     {
         $users = DB::table('users')->get();
 
         return view('users.users', ['users' => $users]);
     }
 
-    public function create(){
-		return view('users.add');
+    public function create()
+    {
+        return view('users.add');
     }
 
-    public function store(Request $request){
-		$scout_name = $request->input('scout_name');
-		$first_name = $request->input('first_name');
-		$last_name = $request->input('last_name');
+    public function store(Request $request)
+    {
+        $scout_name = $request->input('scout_name');
+        $first_name = $request->input('first_name');
+        $last_name = $request->input('last_name');
 
-		$password = $request->input('password');
-		$password_repeat = $request->input('password_repeat');
+        $password = $request->input('password');
+        $password_repeat = $request->input('password_repeat');
 
-		if($password === $password_repeat){
-			$password = Hash::make($password);
-			$password_repeat = null;
+        if ($password === $password_repeat) {
+            $password = Hash::make($password);
+            $password_repeat = null;
 
-			DB::table('users')->insert(['scout_name' => $scout_name,'first_name' => $first_name,'last_name' => $last_name,'password' => $password]);
+            DB::table('users')->insert(['scout_name' => $scout_name, 'first_name' => $first_name, 'last_name' => $last_name, 'password' => $password]);
 
-			return redirect()->back()->with('message', 'Benutzer wurde erstellt');
-		}else{
-			return redirect()->back()->with('error', 'Passwort wurde nicht korrekt wiederholt!');
-		}
+            return redirect()->back()->with('message', 'Benutzer wurde erstellt');
+        } else {
+            return redirect()->back()->with('error', 'Passwort wurde nicht korrekt wiederholt!');
+        }
     }
-
-
 }
--- config/app.php
+++ config/app.php
@@ -160,7 +160,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-	    Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
 
     ],
 
