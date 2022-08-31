653c12c9a75f4b131df1d1dd152d81bffedc37bf
Caspar Brenneisen <github@brenneisen.me>
2022-08-31 18:09:24 +0200

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const { mix } = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -11,6 +11,7 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .version();
+mix.js('resources/js/app.js', 'public/js')
+    .vue({version: 2})
+   .sass('resources/sass/app.scss', 'public/css')
+   .sourceMaps().version();
